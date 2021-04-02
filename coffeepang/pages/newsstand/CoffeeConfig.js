fd2eca4aa7c6925dfe859303efad20ec5fa7273b
hobada96 <hobada96@gmail.com>
2021-04-02 20:16:15 +0900

    link config

---
+++ coffeepang/pages/newsstand/CoffeeConfig.js
@@ -0,0 +1,1 @@
+import React from "react";const CoffeeConfig = (props) => {    console.log(props);    return (        <>            <h1>                Hello            </h1>        </>    )}export default CoffeeConfig;
