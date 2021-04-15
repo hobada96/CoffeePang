7b77a71e9704830afb9e24f134213f525b25faca
hobada96 <hobada96@gmail.com>
2021-04-15 22:29:41 +0900

    add firebase

--- coffeepang/pages/_app.js
+++ coffeepang/pages/_app.js
@@ -1,21 +1,10 @@
-import admin from 'firebase-admin'
+import firebase from "firebase";
 import {createStore} from "redux";
 import {coffeeReducer, wrapper} from "../coffeeRedux"
 import {Provider} from "react-redux";
 const store = createStore(coffeeReducer);
 
 function MyApp({Component, pageProps}) {
-    const firebaseConfig = {
-        apiKey: "AIzaSyDBdTiaW9RjQXf7x6FltzIOX1L11Ek3ZrE",
-        authDomain: "coffeepang-a3a2f.firebaseapp.com",
-        projectId: "coffeepang-a3a2f",
-        storageBucket: "coffeepang-a3a2f.appspot.com",
-        messagingSenderId: "893582678580",
-        appId: "1:893582678580:web:0f3fb11505488b6dba8861",
-        measurementId: "G-WYBKFZQRDS"
-    }
-    console.log(firebase);
-
     return (
         <>
             <Provider store={store}>
